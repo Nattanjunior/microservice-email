32cc3cc3cf0286e1fdb083738d65b37317e8fc1f
nattanjunior <fnatanieljunior@gmail.com>
2025-06-26 01:39:13 -0300

    refactor(core): ajustes no AppModule para integração com novos módulos e fila\n\n- Garante importação correta dos módulos de e-mail e fila\n- Mantém estrutura modular e escalável

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { BullModule } from '@nestjs/bull';
 @Module({
   imports: [
     BullModule.forRoot({ redis: { host: 'redis', port: 6379 }, }),
-    BullModule.registerQueue( { name: 'SEND_EMAIL_QUEUE'}),
     UsersModule,
     SendEmailModule
   ],
