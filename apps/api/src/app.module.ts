e2dd6cb71fdbbfc99f1ee86ae8c839d765d2d3fb
nattanjunior <fnatanieljunior@gmail.com>
2025-06-26 19:50:04 -0300

    refactor(api): atualiza AppModule para integração com Redis e microserviços
    - Adiciona/ajusta importação de módulos relacionados ao Redis
    - Garante integração entre API e microserviço de fila

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
   imports: [
     UsersModule,
+    RedisModule,
   ],
   controllers: [],
   providers: [],
