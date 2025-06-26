b5a0660e905526e9bff48f408f8e6c85e1603788
nattanjunior <fnatanieljunior@gmail.com>
2025-06-26 01:39:50 -0300

    feat(users): integra fila de envio de e-mail no serviço de usuários\n\n- Injeta SendEmailQueueService\n- Envia e-mail de boas-vindas via Redis/Bull ao criar usuário

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { SendEmailService } from 'src/send-email/send-email.service';
-
+import { SendEmailQueueService } from '../send-email/job/send-email-queue/send-email-queue.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly sendEmailQueueService: SendEmailQueueService) { }
 
-  constructor(private readonly sendEmailService: SendEmailService) { }
   async create(createUserDto: CreateUserDto) {
     const { name, email } = createUserDto;
 
-    await this.sendEmailService.handler({ name, email });
+    await this.sendEmailQueueService.execute({ name, email });
 
     return { name, email };
   }
-
 }
